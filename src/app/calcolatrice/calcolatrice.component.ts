9e0571800f007b04d2321d3be99067e1828c952c
Cristiano1782 <cristiano.panizza@marconirovereto.it>
2019-11-10 14:00:32 +0100

    2 files modified

--- src/app/calcolatrice/calcolatrice.component.ts
+++ src/app/calcolatrice/calcolatrice.component.ts
@@ -14,7 +14,34 @@ export class CalcolatriceComponent implements OnInit {
   }
 
   add(numero){
+    var lastChar: string = this.espressione.substr(this.espressione.length -1, this.espressione.length);
+
+    if(this.operatore(numero) && this.operatore(lastChar)){
+      numero = '';
+    }
     this.espressione += numero;
   }
+
+  cancella(){
+    this.espressione = ""
+  }
+
+  uguale(){
+    this.espressione = eval(this.espressione);
+  }
+
+  meno(){
+    this.espressione = this.espressione.substr(0, this.espressione.length -1);
+  }
+
+  inversione(){
+    this.espressione = "-" + this.espressione;
+  }
+
+  operatore(char: string){
+    return char == '+' || char == '/' || char == '*' || char == '-' ? true : false;
+  }
+
+  
 
 }
